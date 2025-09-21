ccfb9e37c71c4abd298884c584b5b9baea1b8a1d
kunj-pandya <kunjpandya998@gmail.com>
2025-09-21 17:28:57 +0530

    implement signUp and validation function

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,11 +1,14 @@
 "use client";
 import Link from "next/link";
-import React from "react";
+import React, { useEffect } from "react";
 import { useRouter } from "next/navigation";
-import { axios } from "axios";
+import axios from "axios";
+import toast from "react-hot-toast";
 
 
-export default function Loginpage() {
+export default function Signuppage() {
+
+    const router = useRouter();
 
     const [user, setUser] = React.useState({
         email: "",
@@ -13,16 +16,63 @@ export default function Loginpage() {
         username: "",
     });
 
+    const [buttonDisabled, setButtonDisabled] = React.useState(false);
+    const [loading, setLoading] = React.useState(false);
+
+    //  validation function
+    const validateForm = () => {
+        if (!user.username || user.username.length < 3) {
+            toast.error("Username must be at least 3 characters");
+            return false;
+        }
+
+        const emailRegex = /\S+@\S+\.\S+/;
+        if (!emailRegex.test(user.email)) {
+            toast.error("Please enter a valid email");
+            return false;
+        }
+
+        if (!user.password || user.password.length < 8) {
+            toast.error("Password must at least 8 characters long");
+            return false;
+        }
+
+        return true;
+    };
+
+
     const onSignup = async () => {
 
+        if (!validateForm()) return;
+
+        try {
+            setLoading(true);
+            const response = await axios.post("/api/users/signup", user);
+            toast.success("Signup successfull!");
+            console.log("Signup suceess", response.data);
+            router.push("/login");
+        } catch (error: any) {
+            console.log("Sign Up Failed", error.message);
+            toast.error(error.response?.data?.error || error.message);
+        } finally {
+            setLoading(false);
+        }
     };
+
+    useEffect(() => {
+        if (user.email.length > 0 && user.password.length > 0 && user.username.length > 0) {
+            setButtonDisabled(false);
+        } else {
+            setButtonDisabled(true)
+        }
+    }, [user]);
 
 
     return (
         <div className="flex items-center justify-center min-h-screen bg-zinc-900 border-zinc-400">
             <div className="w-full max-w-md bg-zinc-800 shadow-lg rounded-2xl p-8">
                 <h1 className="text-2xl font-semibold text-center text-orange-400 mb-6">
-                    Sign Up
+                    {loading ? "Proccessing...." : "Sign up"}
                 </h1>
                 <hr className="mb-6 border-zinc-700" />
 
@@ -77,9 +127,14 @@ export default function Loginpage() {
                 {/* Button */}
                 <button
                     onClick={onSignup}
-                    className="w-full bg-orange-500 text-white py-3 rounded-lg font-medium hover:bg-orange-600 cursor-pointer transition-colors mb-4"
+                    disabled={buttonDisabled || loading}
+                    className={`w-full py-3 rounded-lg font-medium transition-colors mb-4
+            ${buttonDisabled || loading
+                            ? "bg-gray-500 cursor-not-allowed"
+                            : "bg-orange-500 hover:bg-orange-600 text-white cursor-pointer"
+                        }`}
                 >
-                    Sign Up
+                    {buttonDisabled ? "Fill all fields" : loading ? "Signing up..." : "Sign up"}
                 </button>
 
                 {/* Link */}
