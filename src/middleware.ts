8a133be6d2f1821b1b3b6741b65b7a1d93054655
kunj-pandya <kunjpandya998@gmail.com>
2025-10-03 09:47:55 +0530

    middleware for protected routes

---
+++ src/middleware.ts
@@ -0,0 +1,30 @@
+import { NextResponse } from 'next/server'
+import { NextRequest } from 'next/server'
+
+
+export function middleware(request: NextRequest) {
+    const path = request.nextUrl.pathname
+
+    const publicPath = path === "/login" || path === "/signup";
+
+    const token = request.cookies.get('token')?.value || '';
+
+    if (publicPath && token) {
+        return NextResponse.redirect(new URL('/', request.nextUrl));
+    }
+
+    if (!publicPath && !token) {
+        return NextResponse.redirect(new URL('/login', request.nextUrl));
+    }
+}
+
+
+
+export const config = {
+    matcher: [
+        '/',
+        '/profile/:path*',
+        '/login',
+        '/signup'
+    ],
+}
